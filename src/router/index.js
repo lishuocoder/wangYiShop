1881837d45a5f402a27ef3f1520f8661a52c4404
lishuo <lishuocoder@163.com>
2020-04-05 21:34:31 +0800

    第一次提交项目

---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Category from '@/views/category'
+import categorylist from '@/views/categorylist'
+import product from '@/views/product'
+import buycart from '@/views/buycart'
+import About from '@/views/About'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component:About
+  },
+  {
+    path:'/category',
+    name:'category',
+    component:Category
+
+  },{
+    path:'/categorylist/:id',
+    name:'categorylist',
+    component: categorylist,
+    props:true
+  },{
+    path:"/product/:id",
+    name:"product",
+    component:product,
+    props:true
+  },{
+    path:"/buycart",
+    name:"buycart",
+    component:buycart
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+	<tabBtn></tabBtn>
+  </div>
+</template>
+
+<script>
+	import tabBtn from '@/components/tabBtn.vue';
+	
+	export default{
+		data(){
+			return{}
+		},
+		components:{
+			tabBtn
+		}
+	}
+</script>
